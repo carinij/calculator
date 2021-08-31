052e4db0b1fa141687a6c9ca542b0ac6731ca972
carinij <carinij@gmail.com>
2021-08-31 09:56:43 -0700

    Initial setup

---
+++ src/client/app.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import "./styles.css";
+
+export default function Calculator () {
+  return (
+    <h1>
+      Hello, world!
+    </h1>
+  )
+}
---
+++ webpack.common.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/client/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "public/"),
+    publicPath: "/public/",
+    filename: "calculator.js"
+  }
+};
