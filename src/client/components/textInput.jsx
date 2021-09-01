e990ccad3b53efe57c00c071d4afe50871ea6f19
carinij <carinij@gmail.com>
2021-09-01 11:37:04 -0700

    Decided to do validation on submit (rather than per character) after all. Refactored accordingly.

--- src/client/components/textInput.jsx
+++ src/client/components/textInput.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function TextInput(props) {
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    props.setInputDisplay(e.target.value);
+  }
+
   return (
     <div>
-      <input type="text" id="text-input" name="text-input" placeholder="Enter formula" />
+      <input type="text" id="text-input" name="text-input" placeholder="Enter formula" value={props.inputDisplay} onChange={handleChange}/>
     </div>
   )
 }
