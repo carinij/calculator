1e88b58191d864780ebd3ca25f4427408688d332
carinij <carinij@gmail.com>
2021-09-01 11:33:29 -0700

    Very minor changes.

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -16,7 +16,7 @@ export default function Calculator () {
   const validateFormula = (str) => {
     // ^ indicates beginning of string, and $ end of string; [] enclose all the characters to be matched; \ escapes
     // So this regex will return true in a test only if each character in the test's argument string
-    // matches 0-9 or one of the other listed characters
+    // matches 0-9 or one of the other listed characters.
     const okCharsRegex = /^[0-9\+\-\*\/\(\)\.]+$/;
     if (!okCharsRegex.test(str)) {
       updateResultDisplay('Validation failed. Please remove any spaces or characters other than 0-9, ".", "+", "-", "*", "/".');
@@ -30,11 +30,12 @@ export default function Calculator () {
       updateResultDisplay('Validation failed. Please check for extraneous operators or missing numbers.');
       return false;
     }
-    return true;
 
     // To do: check for starting or ending with operators
 
     // To do: check each open parens has a close parens
+
+    return true;
   }
 
   const updateResultDisplay= (str) => {
