fb76a8b17e3d8b53c35c1365dbfc240624949cb8
carinij <carinij@gmail.com>
2021-09-01 11:34:19 -0700

    Removed extraneous code.

--- src/client/components/calculatorButtons.jsx
+++ src/client/components/calculatorButtons.jsx
@@ -2,16 +2,10 @@ import React from 'react';
 
 export default function CalculatorButtons(props) {
 
-  const buttonDisplayArray = ['+', '-', '*',  '/', '7', '8', '9', 'AC', '4', '5', '6', 'del', '1', '2', '3', '.', '0', '='];
-  const buttonNameArray = ['plus', 'minus', 'times', 'divide', 'seven', 'eight', 'nine', 'AC', 'four', 'five', 'six', 'del', 'one', 'two', 'three', 'dot', 'zero', 'equals'];
-  let buttonInfoArray = [];
+  const buttonNameArray = ['+', '-', '*',  '/', '7', '8', '9', 'AC', '4', '5', '6', 'del', '1', '2', '3', '.', '0', '(', ')', '='];
 
-  for (let i = 0; i < buttonDisplayArray.length; i++) {
-    buttonInfoArray.push({displayName: buttonDisplayArray[i], name: buttonNameArray[i]});
-  }
-
-  const buttons = buttonInfoArray.map((item) =>
-    <button key={"btn-" + item.name} id={"btn-" + item.name} className="calculator-button">{item.displayName}</button>
+  const buttons = buttonNameArray.map((item) =>
+    <button key={"btn-" + item} id={"btn-" + item} className="calculator-button" onClick={props.clickHandler}>{item}</button>
   )
 
   return (
