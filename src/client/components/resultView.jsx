4ce1fec254ffe60a2b2e2e997d161b2cef4c6474
carinij <carinij@gmail.com>
2021-09-01 11:36:16 -0700

    Added clickHandler to "Clear History" button (not yet tested).

--- src/client/components/resultView.jsx
+++ src/client/components/resultView.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 export default function ResultView(props) {
+
+  const clickHandler = (e) => {
+    e.preventDefault();
+    props.setResultDisplay("");
+  }
+
   return (
     <div className="result-view-container">
-      <textarea id="result-view" name="result-view" placeholder="Results will be displayed here" readOnly></textarea>
+      <textarea id="result-view" name="result-view" placeholder="Results will be displayed here" value={props.resultDisplay} readOnly onClick={clickHandler}></textarea>
       <button className="btn-clear-history">Clear History</button>
     </div>
   )
