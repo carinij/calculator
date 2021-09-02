648ca92633e042ff851a5ce1cc377b1d14b6c1c2
carinij <carinij@gmail.com>
2021-09-02 13:27:52 -0700

    Added a call to calc

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 
-const validateFormula = require('./validateFormula.js')
+const validateFormula = require('./validateFormula.js');
+const calc = require('./calc.js');
 
 const host = 'localhost';
 const port = '3000';
@@ -9,12 +10,14 @@ const port = '3000';
 app.use(express.static('public'));
 
 app.get('/calc', (req, res) => {
-  if (req.query.formula.length > 1024) {
+  let formula = req.query.formula;
+  if (formula.length > 1024) {
     res.status(400).send("Error: Query length exceeds 1024 characters.");
   }
-  const val = validateFormula(req.query.formula);
+  const val = validateFormula(formula);
   if (val.outcome) {
-    res.send({answer: "Probably it's 5?"});
+    const answer = calc(formula);
+    res.send({answer: answer});
   } else {
     res.status(400).send({answer: val.text });
   }
