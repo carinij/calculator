563986309ea85dae6d5fcc3b32df24b429c23c71
carinij <carinij@gmail.com>
2021-09-01 19:52:21 -0700

    Minor refactor to accommodate theoretical future environment variables. Changed response to JSON object.

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+const host = 'localhost';
+const port = '3000';
 
 app.use(express.static('public'));
 
@@ -8,10 +10,10 @@ app.get('/calc', (req, res) => {
   if (req.query.formula.length > 1024) {
     res.status(400).send("Error: Query length exceeds 1024 characters.");
   }
-  console.log(req.query);
-  res.send("Hello there!");
+  console.log(req.query.formula);
+  res.send({answer: "Probably it's 5?"});
 });
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+  console.log(`Listening at http://${host}:${port}`);
 });
