f444089e75812b3809ce33674035814c431484b3
carinij <carinij@gmail.com>
2021-09-04 18:18:08 -0700

    Passing equalsMode and setEqualsMode through to TextInput

--- client/components/inputView.jsx
+++ client/components/inputView.jsx
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import TextInput from './textInput.jsx';
 import CalculatorButtons from './calculatorButtons.jsx';
 
 export default function InputView(props) {
 
-  const [inputDisplay, setInputDisplay] = useState("");
+  const inputDisplay = props.inputDisplay;
+  const setInputDisplay = props.setInputDisplay;
 
   const clickHandler = (e) => {
     e.preventDefault();
     // This click handler is attached to elements with ids "btn-" and then a value matching what we're displaying to the user
-    // By taking everything from character 4 on, "target" is being set to that value
+    // By taking everything from the character at index 4 on, "target" is being set to that value
     const target = e.target.id.substring(4);
     let currentInput;
 
@@ -32,7 +33,13 @@ export default function InputView(props) {
 
   return (
     <div className="input-view-container">
-      <TextInput inputDisplay={inputDisplay} setInputDisplay={setInputDisplay} setResultDisplay={props.setResultDisplay}/>
+      <TextInput
+        inputDisplay={inputDisplay}
+        setInputDisplay={setInputDisplay}
+        setResultDisplay={props.setResultDisplay}
+        equalsMode={props.equalsMode}
+        setEqualsMode={props.setEqualsMode}
+      />
       <CalculatorButtons clickHandler={clickHandler}/>
     </div>
   )
